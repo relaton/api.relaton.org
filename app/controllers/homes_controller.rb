2de5038bb9fe2a33ae3d6dcf28897b359e225a29
Abu Nashir <abunashir@gmail.com>
2019-10-09 16:03:31 +0600

    Add basic intro page as the root

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,20 @@
+class HomesController < ApplicationController
+  def show
+    render json: intro_message, status: :ok
+  end
+
+  private
+
+  def intro_message
+    {
+      status: :ok,
+      author: "Rebose Inc.",
+      message: "This is an API only application",
+
+      links: {
+        github: "https://github.com/relaton/api.relaton.org",
+        documentation: "https://github.com/relaton/api.relaton.org/wiki",
+      }
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "homes#show"
+
   namespace :api do
     resource :standard, only: [:show]
   end
