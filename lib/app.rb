a4dcb830e90e1cd1b8ced4d3eee1dd1a33c637fa
Andrei Kislichenko <android.2net@gmail.com>
2021-07-05 17:46:47 +0200

    remove puts & uncomment path checking

--- lib/app.rb
+++ lib/app.rb
@@ -4,12 +4,7 @@ module Relaton
   class Api
     class << self
       def handler(event:, context: {})
-        puts "123 called Relaton"
-        puts ENV["AWS_BUCKET"]
-        puts event
-        puts event["httpMethod"]
-
-        if event["httpMethod"] == "GET" # && event["path"] == "/api/v1/document"
+        if event["httpMethod"] == "GET" && event["path"].match?(/\/api\/v1\/document$/)
           unless event["queryStringParameters"]["code"]
             return bad_request "Parametr 'code' is required."
           end
@@ -25,30 +20,11 @@ module Relaton
 
       private
 
-      # def headers
-      #   # cors_origin = ENV["DEFAULT_ORIGIN"]
-      #   # input_origin = event.fetch("headers", {}).fetch("origin", "")
-
-      #   # if /#{ENV["CORS_ORIGIN_REGEX"]}/.match? input_origin
-      #   #   cors_origin = input_origin
-      #   # end
-
-      #   {
-      #     "Access-Control-Allow-Origin" => "*",
-      #     "Access-Control-Allow-Headers" => "Content-Type",
-      #     "Access-Control-Allow-Methods" => "GET",
-      #   }
-      # end
-
       def fetch(env)
         item = Relaton::Finder.instance.fetch(*params(env))
         return not_found "Document not found." unless item
 
-        puts "123 before respond. Item: #{item.id}"
-
         xml = item.to_xml bibdata: true
-
-        puts "123 XML: #{xml[0..20]}"
 
         {
           statusCode: 200,
